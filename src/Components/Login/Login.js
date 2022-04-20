e32cee483757373edeaa4f31aaaaea66342fa37f
Habibullah800 <mdhabibullah800@gmail.com>
2022-04-20 16:08:28 +0600

    log-in register create

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,19 +1,37 @@
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import React, { useRef } from 'react';
+import { Form } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
+import app from '../../firebase.init';
 import useFirebase from '../../Hooks/UseFirebase';
 import './Login.css'
+const auth = getAuth(app);
 const Login = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('');
     const navigate = useNavigate();
 
+    const { user, handleSignOut } = useFirebase()
+
     const handleSubmit = event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
+
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+            })
+            .catch(error => {
+                console.error(error)
+            })
+        // Email and password
         console.log(email, password);
 
     }
+
 
 
     const { signInWithGoogle } = useFirebase();
@@ -26,20 +44,42 @@ const Login = () => {
     return (
         <div className='login-container'>
             <h3>Please Login</h3>
-            <div>
-                <button className='btn btn-primary' onClick={signInWithGoogle}> Sign-in with Google</button>
+
+
+            {
+                user?.uid
+                    ?
+                    <div className='mb-4' >
+                        <Link className='btn btn-primary' to='/placeOrder'> Take our Treatment</Link>
+                    </div>
+                    :
+                    <div>
+                        <button className='btn btn-primary' onClick={signInWithGoogle}> Sign-in with Google</button>
+                        <br></br>
+                        <br></br>
+                    </div>
+            }
+
+            <Form onSubmit={handleSubmit}>
+                <Form.Group>
+                    <input ref={emailRef} type='text' placeholder='Your Email' required></input>
+                    <br></br>
+                    <br></br>
+                </Form.Group>
+
+
+                <Form.Group>
+                    <input ref={passwordRef} type='password' placeholder='Password' required></input>
+                    <br></br>
+                </Form.Group>
                 <br></br>
-                <br></br>
-            </div>
-            <form onSubmit={handleSubmit}>
-                <input ref={emailRef} type='text' placeholder='Your Email' required></input>
-                <br></br>
-                <br></br>
-                <input ref={passwordRef} type='password' placeholder='Password' required></input>
-                <br></br>
-                <br></br>
+
+
+
                 <input type='submit' value='Login' ></input>
-            </form>
+
+            </Form>
+
             <p className='mt-4'> New to our Telemedicine Service ? <br></br>
                 <Link to='/register' className='text-danger pe-auto text-decoration-none' onClick={navigateRegister}>Please Register</Link>
             </p>
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Register.css'
 import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import RequireAuth from '../RequireAuth/RequireAuth';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import app from '../../firebase.init';
+
+const auth = getAuth(app);
+
 
 const Register = () => {
+    // const auth = getAuth(app);
+    // const [
+    //     createUserWithEmailAndPassword,
+    //     user,
+    //     loading,
+    //     error,
+    // ] = useCreateUserWithEmailAndPassword(auth);
+
     const navigate = useNavigate();
     const navigateLogin = () => {
         navigate('/login')
     }
 
+    // if (user) {
+    //     navigate('/home')
+    // }
+    const [error, setError] = useState([]);
+
+
     const register = event => {
         event.preventDefault();
-        const name = event.target.name.value
         const email = event.target.email.value
         const password = event.target.password.value
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+
+
+            })
+            .catch((error) => {
+                const errorMessage = error.message;
+                setError(errorMessage)
+            }
+
+            )
     }
+
     return (
         <div className='register-container'>
 
@@ -31,9 +64,13 @@ const Register = () => {
                 <br></br>
                 <input type='submit' value='Register'></input>
             </form>
+            <br></br>
+            <p className='text-danger'><small> {error}</small>
+            </p>
             <p className='mt-4'> Already have an account ? <br></br>
                 <Link to='/login' className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link>
             </p>
+
         </div>
     );
 };
